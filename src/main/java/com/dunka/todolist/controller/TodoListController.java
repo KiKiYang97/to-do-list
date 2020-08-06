903ce51a7b637c313cea998721d198f906bae36a
KiKiYang97 <dunka97@163.com>
2020-08-06 11:07:57 +0800

    [Kiki]feature :implements methods in controller.

--- src/main/java/com/dunka/todolist/controller/TodoListController.java
+++ src/main/java/com/dunka/todolist/controller/TodoListController.java
@@ -1,7 +1,35 @@
 package com.dunka.todolist.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.dunka.todolist.model.Todo;
+import com.dunka.todolist.service.TodoListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/todos")
 public class TodoListController {
+    @Autowired
+    private TodoListService todoListService;
+
+    @GetMapping("/")
+    public List<Todo> findAllTodoList() {
+        return todoListService.findAll();
+    }
+
+    @PostMapping("/")
+    public Todo addTodo(@RequestBody Todo todo) {
+        return todoListService.save(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@RequestBody Todo newTodo,@PathVariable Integer id) {
+        return todoListService.putTodo(id,newTodo);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTodo(@PathVariable Integer id) {
+        return todoListService.deleteById(id);
+    }
 }
